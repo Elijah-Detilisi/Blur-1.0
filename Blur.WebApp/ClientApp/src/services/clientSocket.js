73e5385d1e1e64ad57d7c77ed946ff2ec4a4929b
Philani Mbatha <PhilaniM@hollywoodbets.net>
2022-11-29 11:49:33 +0200

    add socket client to web app

--- Blur.WebApp/ClientApp/src/services/clientSocket.js
+++ Blur.WebApp/ClientApp/src/services/clientSocket.js
@@ -1,18 +1,21 @@
-export default function App() {
+﻿class ClientSocket {
+    //fields
+    socketService = "/Echo";
+    socketUri = "ws://127.0.0.1:5000" + socketService;
+    clientSocket = new WebSocket(socketUri);
 
-    let socket = new WebSocket("ws://127.0.0.1:5000/Echo");
-
-    socket.onopen = function (e) {
+    //methods
+    this.socket.onopen = function (e) {
         alert("[open] Connection established");
         alert("Sending to server");
         socket.send("My name is John");
     };
 
-    socket.onmessage = function (event) {
+    this.socket.onmessage = function (event) {
         alert(`[message] Data received from server: ${event.data}`);
     };
 
-    socket.onclose = function (event) {
+    this.socket.onclose = function (event) {
         if (event.wasClean) {
             alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
         } else {
@@ -22,15 +25,7 @@ export default function App() {
         }
     };
 
-    socket.onerror = function (error) {
+    this.socket.onerror = function (error) {
         alert(`[error]`);
     };
-
-    return (
-        <div className="bg-gray-400">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>        
-    )
 }
